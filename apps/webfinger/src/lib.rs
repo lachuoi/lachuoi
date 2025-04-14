55887362d491547ac880fc61bb395dbe32c89028
Seungjin Kim <seungjin@duck.com>
2025-04-14 14:12:38 +0700

    WIP: webfinger dev

--- apps/webfinger/src/lib.rs
+++ apps/webfinger/src/lib.rs
@@ -3,12 +3,13 @@ use spin_sdk::{
     http::{IntoResponse, Params, Request, Response, Router},
     http_component,
 };
-use std::env;
 
 /// A simple Spin HTTP component.
 #[http_component]
 async fn handle_root(req: Request) -> Result<impl IntoResponse> {
-    println!("Handling request to {:?}", req.header("spin-full-url"));
+    println!("{:?}", req.query());
+    println!("???");
+
     Ok(Response::builder()
         .status(200)
         .header("content-type", "text/plain")
