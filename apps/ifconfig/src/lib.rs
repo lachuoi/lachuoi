19d9008156505654b6e1485c5375e1dbcd9c466c
Seungjin Kim <seungjin@duck.com>
2025-04-15 20:14:33 +0700

    ifconfig now with country and city(geoip)

--- apps/ifconfig/src/lib.rs
+++ apps/ifconfig/src/lib.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
+use serde_json::Value;
 use spin_sdk::{
-    http::{IntoResponse, Request, Response},
+    http::{IntoResponse, Method::Get, Request, Response},
     http_component,
 };
-use std::net::{IpAddr, SocketAddr};
+use std::str;
 
 /// A simple Spin HTTP component.
 #[http_component]
@@ -17,10 +18,39 @@ async fn handle_root(req: Request) -> Result<impl IntoResponse> {
         None => spin_client_addr,
     };
 
+    let (country, city) = match geoip(x_forwarded_for).await? {
+        Some(x) => {
+            let city = x
+                .get("city")
+                .unwrap()
+                .get("names")
+                .unwrap()
+                .get("en")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_string();
+            let country = x
+                .get("country")
+                .unwrap()
+                .get("names")
+                .unwrap()
+                .get("en")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_string();
+            (country, city)
+        }
+        None => ("".to_string(), "".to_string()),
+    };
+
+    let txt_output = format!("{}\n{}\n{}", x_forwarded_for, country, city);
+
     Ok(Response::builder()
         .status(200)
         .header("content-type", "text/plain")
-        .body(x_forwarded_for.to_string())
+        .body(txt_output.trim())
         .build())
 }
 
@@ -32,4 +62,25 @@ async fn whoyouare(req: &Request) -> Result<()> {
     }
     println!("{:?}", method);
     Ok(())
+}
+
+async fn geoip(ip: &str) -> Result<Option<Value>> {
+    let uri = format!("http://localhost:3000/geoip?{}", ip);
+    // let uri = format!("http://geoip.spin.internal/geoip?{}", ip);
+    let request = Request::builder().method(Get).uri(uri).build();
+    let response: Response = spin_sdk::http::send(request).await?;
+
+    if response.status() == &406u16 {
+        return Ok(None);
+    }
+
+    match response.status() {
+        200u16 => {
+            let response_body = str::from_utf8(response.body()).unwrap();
+            let v: Value = serde_json::from_str(response_body).unwrap();
+            Ok(Some(v))
+        }
+        406u16 => Ok(None),
+        _ => Ok(None),
+    }
 }
